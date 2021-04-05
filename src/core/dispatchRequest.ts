4bd253689952ea1175f20dada29898086dc83c2f
ihoneys <thefrontliu@163.com>
2021-04-05 23:21:22 +0800

    interceptor拦截器

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -1,37 +1,34 @@
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types/index'
 import xhr from './xhr'
 import { buildURL } from '../helpers/url'
 import { transformRequest, transformResponse } from '../helpers/data'
 import { processHeaders } from '../helpers/headers'
-
 export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
+    processConfig(config)
+    return xhr(config).then(res => transformResponseData(res))
 }
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
+function processConfig(config: AxiosRequestConfig) {
+    config.url = transformURL(config)
+    config.headers = transformHeaders(config)
+    config.data = transformRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url!, params)
+    const { url, params } = config
+    return buildURL(url!, params)
 }
 
 function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
+    return transformRequest(config.data)
 }
 
 function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
+    const { headers = {}, data } = config
+    return processHeaders(headers, data)
 }
 
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
+    res.data = transformResponse(res.data)
+    return res
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,10 +5,12 @@ export type Method = 'get' | 'GET'
     | 'patch' | 'PATCH'
     | 'options' | 'OPTIONS'
     | 'head' | 'HEAD'
+    | 'delete' | 'DELETE'
+
 
 
 export interface AxiosRequestConfig {
-    url: string,
+    url?: string,
     method?: Method,
     data?: any,
     params?: any,
@@ -17,8 +19,8 @@ export interface AxiosRequestConfig {
     timeout?: number
 }
 
-export interface AxiosResponse {
-    data: any,
+export interface AxiosResponse<T = any> {
+    data: T,
     status: number,
     statusText: string,
     headers: any,
@@ -26,7 +28,7 @@ export interface AxiosResponse {
     request: any,
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {
 
 }
 
@@ -36,4 +38,47 @@ export interface AxiosError extends Error {
     request: any,
     response: AxiosResponse,
     isAxiosError: boolean
+}
+
+export interface Axios {
+    interceptors: {
+        request: AxiosInterceptorManager<AxiosRequestConfig>
+        response: AxiosInterceptorManager<AxiosResponse>
+    }
+
+    request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+    get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInstance extends Axios {
+    <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+    <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+
+export interface AxiosInterceptorManager<T> {
+    use(resolve: ResolvedFn<T>, reject?: RejectedFn): number
+    eject(id: number): void
+}
+
+export interface ResolvedFn<T> {
+    (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+    (error: any): any
 }
